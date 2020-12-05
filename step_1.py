db16ebc425072b33ac6c40d9422292aae652075f
Galart <galart086@mail.ru>
2020-12-05 12:36:35 +0500

    step_1

---
+++ step_1.py
@@ -0,0 +1,20 @@
+games = ['Far Cry 5', 'FIFA 21', 'GTA V', 'Need for Speed Heat', 'Hearts of Iron IV']
+print(games)
+
+games.append('Fortnite')
+print(games)
+
+games.insert(3, 'God of War 2')
+print(games)
+
+game = games.pop(2)
+print(game)
+
+game = games.index('Far Cry 5')
+print(game)
+
+games.append('Far Cry 5')
+print(games)
+
+game = games.count('Far Cry 5')
+print(game)
